a3710a49c791c8414b3298fe161c94ac667622af
ioio_lindoca <iohanna_oliveira@estudante.sesisenai.org.br>
2026-09-18 21:58:05 -0300

    Update Estoque.java

---
+++ Estoque.java
@@ -0,0 +1,13 @@
+import java.util.list;
+
+public class Estoque {
+    List<Produto> produtos;
+
+    public void ListarProdutos{
+        for(Produto produto : produto){
+            if(pro.getId() == id){
+                System.out.println()
+            }
+        }
+    }
+}
